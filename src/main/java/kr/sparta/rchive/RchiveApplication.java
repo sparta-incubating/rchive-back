0409f782899a07eac5189d3db4401538a2a1367e
jiisuniui <dudndi9752@gmail.com>
2024-06-13 15:54:46 +0900

    [#1] feat: swagger 설정

--- src/main/java/kr/sparta/rchive/RchiveApplication.java
+++ src/main/java/kr/sparta/rchive/RchiveApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableScheduling
 @EnableJpaAuditing
+@EnableWebSecurity
 @SpringBootApplication
 public class RchiveApplication {
 
---
+++ src/main/java/kr/sparta/rchive/global/config/SwaggerConfig.java
@@ -0,0 +1,70 @@
+package kr.sparta.rchive.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.List;
+import java.util.function.Predicate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    private Docket testDocket(String groupName, Predicate<String> selector) {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .securityContexts(List.of(this.securityContext()))
+                .securitySchemes(List.of(this.apiKey()))
+                .groupName("testApi")
+                .select()
+                .apis(RequestHandlerSelectors.
+                        basePackage("rchive"))
+                .paths(PathSelectors.ant("/v1/**")).build();
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("Authorization", authorizationScopes));
+    }
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+
+    // swagger token 인증 API
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "BearerAuth";
+
+        return new OpenAPI()
+                .info(new Info().title("Example API").version("v1"))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName));
+
+    }
+}
+
