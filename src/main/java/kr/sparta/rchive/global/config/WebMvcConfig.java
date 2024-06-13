2d5d111d85fe371bb12fbd368e6cdc5285ffc9fc
jiisuniui <dudndi9752@gmail.com>
2024-06-13 16:00:13 +0900

    [#1] feat: cors 설정

---
+++ src/main/java/kr/sparta/rchive/global/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package kr.sparta.rchive.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://rchive.kr", "https://rchive.kr")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
