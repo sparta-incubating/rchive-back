f32880e7536558328294a127cc02c37b8dc17268
jiisuniui <dudndi9752@gmail.com>
2024-06-20 17:51:29 +0900

    [#15] chore(Role): 필요없는 import 삭제

--- src/main/java/kr/sparta/rchive/domain/user/entity/Role.java
+++ src/main/java/kr/sparta/rchive/domain/user/entity/Role.java
@@ -5,18 +5,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import kr.sparta.rchive.domain.user.enums.AuthEnum;
-import kr.sparta.rchive.domain.user.enums.GenderEnum;
 import kr.sparta.rchive.domain.user.enums.RoleEnum;
 import kr.sparta.rchive.global.entity.BaseTimeEntity;
 import lombok.AccessLevel;
@@ -24,7 +18,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Table(name="tb_role")
