b3984c7a7c8b19f82fa55a3cb4ca2097b0958f92
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-06-20 16:45:01 +0900

    [#2] feat: Education Data Entity 관련 EducationTypeEnum 추가

---
+++ src/main/java/kr/sparta/rchive/domain/educationdata/enums/EducationTypeEnum.java
@@ -0,0 +1,10 @@
+package kr.sparta.rchive.domain.educationdata.enums;
+
+public enum EducationTypeEnum {
+    Sparta_Lecture,
+    Special_Lecture,
+    Level_Challenge,
+    Level_Standard,
+    Level_Basic,
+    Project_Description
+}
