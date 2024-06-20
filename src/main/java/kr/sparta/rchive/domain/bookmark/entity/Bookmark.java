27447e13f4bcafc79d58c56a1b314e19f69ec01f
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-06-20 17:46:20 +0900

    [#18] feat(Bookmark): Bookmark Entity 생성

---
+++ src/main/java/kr/sparta/rchive/domain/bookmark/entity/Bookmark.java
@@ -0,0 +1,36 @@
+package kr.sparta.rchive.domain.bookmark.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import kr.sparta.rchive.domain.educationData.entity.EducationData;
+import kr.sparta.rchive.domain.educationData.entity.EducationDataTrackId;
+import kr.sparta.rchive.domain.user.entity.User;
+import kr.sparta.rchive.global.entity.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_Bookmark")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@IdClass(BookmarkId.class)
+public class Bookmark extends BaseTimeEntity {
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "education_data_id")
+    private EducationData educationData;
+}
---
+++ src/main/java/kr/sparta/rchive/domain/bookmark/entity/BookmarkId.java
@@ -0,0 +1,10 @@
+package kr.sparta.rchive.domain.bookmark.entity;
+
+import java.io.Serializable;
+import lombok.Getter;
+
+@Getter
+public class BookmarkId implements Serializable {
+    private Long user;
+    private Long educationData;
+}
