add74cc1d94fae434f766404061e6b08c066a4b2
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-06-21 17:44:07 +0900

    [#24] fix(Tag): EducationDataRes @Builder

--- src/main/java/kr/sparta/rchive/domain/post/dto/response/EducationDataRes.java
+++ src/main/java/kr/sparta/rchive/domain/post/dto/response/EducationDataRes.java
@@ -1,7 +1,9 @@
 package kr.sparta.rchive.domain.post.dto.response;
 
 import java.time.LocalDate;
+import lombok.Builder;
 
+@Builder
 public record EducationDataRes(
         String title,
         String tutor,
