fc8a783cde2ac700dae649df0a458a4fb8550441
jiisuniui <dudndi9752@gmail.com>
2024-06-21 15:44:27 +0900

    [#10] feat(User): UserRepository 수정

--- src/main/java/kr/sparta/rchive/domain/user/repository/UserRepository.java
+++ src/main/java/kr/sparta/rchive/domain/user/repository/UserRepository.java
@@ -1,10 +1,18 @@
 package kr.sparta.rchive.domain.user.repository;
 
+import java.util.Optional;
 import kr.sparta.rchive.domain.user.entity.User;
+import kr.sparta.rchive.domain.user.enums.OAuthTypeEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 
+    Boolean existsByEmailAndOAuthType(String email, OAuthTypeEnum oAuthTypeEnum);
+
+    Optional<User> findByEmailAndOAuthType(String email, OAuthTypeEnum oAuthTypeEnum);
+
+    Optional<User> findByEmailAndRefreshToken(String email, String refreshToken);
 }
