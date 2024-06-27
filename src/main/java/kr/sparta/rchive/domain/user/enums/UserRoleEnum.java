1d49e61b9c494894448e1246a54c2aff19d5ebed
jiisuniui <dudndi9752@gmail.com>
2024-06-27 10:20:37 +0900

    [#10] feat(User): user_role 추가

--- src/main/java/kr/sparta/rchive/domain/user/enums/UserRoleEnum.java
+++ src/main/java/kr/sparta/rchive/domain/user/enums/UserRoleEnum.java
@@ -1,12 +1,13 @@
 package kr.sparta.rchive.domain.user.enums;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+
 
 @Getter
 public enum UserRoleEnum {
 
     USER(Authority.USER),  // 사용자 권한
+    MANAGER(Authority.MANAGER),  // 사용자 권한
     ADMIN(Authority.ADMIN);  // 관리자 권한
 
     private final String authority;
@@ -15,12 +16,9 @@ public enum UserRoleEnum {
         this.authority = authority;
     }
 
-    public String getAuthority() {
-        return this.authority;
-    }
-
     public static class Authority {
         public static final String USER = "ROLE_USER";
+        public static final String MANAGER = "ROLE_MANAGER";
         public static final String ADMIN = "ROLE_ADMIN";
     }
 
