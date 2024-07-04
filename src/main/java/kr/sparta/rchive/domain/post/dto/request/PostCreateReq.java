30522e525bf00242a3238a13e1c2af7374b5dfd5
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-07-04 15:26:50 +0900

    [#43] feat(Post): Post 생성 시 필요한 Request 추가

--- src/main/java/kr/sparta/rchive/domain/post/dto/request/PostCreateReq.java
+++ src/main/java/kr/sparta/rchive/domain/post/dto/request/PostCreateReq.java
@@ -1,5 +1,6 @@
 package kr.sparta.rchive.domain.post.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.time.LocalDate;
 import java.util.List;
 import kr.sparta.rchive.domain.post.enums.PostTypeEnum;
@@ -7,14 +8,16 @@ import kr.sparta.rchive.domain.user.enums.TrackEnum;
 import lombok.Builder;
 
 @Builder
+@JsonIgnoreProperties
 public record PostCreateReq(
         PostTypeEnum postType,
         String title,
         String tutor,
         LocalDate uploadedAt,
-        String video,
+        String videoLink,
+        String contentLink,
         String content,
-        List<String> tagNames,
+        List<String> tagNameList,
         TrackEnum track,
         int period
 ) {
