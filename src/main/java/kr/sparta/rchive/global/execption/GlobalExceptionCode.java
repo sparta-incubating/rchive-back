c64d4d3762f46692c5688d1c41d4f84b95353a73
jiisuniui <dudndi9752@gmail.com>
2024-07-05 15:43:31 +0900

    [#42] refactor(GlobalException): 안 쓰는 부분 삭제

--- src/main/java/kr/sparta/rchive/global/execption/GlobalExceptionCode.java
+++ src/main/java/kr/sparta/rchive/global/execption/GlobalExceptionCode.java
@@ -11,9 +11,9 @@ public enum GlobalExceptionCode {
     /*  400 BAD_REQUEST : 잘못된 요청  */
     BAD_REQUEST_INVALID_VALUE(HttpStatus.BAD_REQUEST, "GLOBAL-001", "유효하지 않은 값"),
     BAD_REQUEST_INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "GLOBAL-002", "파라미터 누락"),
-    BAD_REQUEST_REFRESH_TOKEN_NULL(HttpStatus.BAD_REQUEST,"GLOBAL-007","리프레시 토큰 없음"),
-    BAD_REQUEST_REFRESH_NOT_MATCH(HttpStatus.BAD_REQUEST,"GLOBAL-008","리프레시 토큰이 일치하지 않음"),
-    BAD_REQUEST_TOKEN_EXPIRED(HttpStatus.BAD_REQUEST,"GLOBAL-009","토큰 시간 만료"),
+    BAD_REQUEST_REFRESH_TOKEN_NULL(HttpStatus.BAD_REQUEST,"GLOBAL-006","리프레시 토큰 없음"),
+    BAD_REQUEST_REFRESH_NOT_MATCH(HttpStatus.BAD_REQUEST,"GLOBAL-007","리프레시 토큰이 일치하지 않음"),
+    BAD_REQUEST_TOKEN_EXPIRED(HttpStatus.BAD_REQUEST,"GLOBAL-008","토큰 시간 만료"),
 
     /*  401 UNAUTHORIZED : 인증 안됨  */
 
@@ -24,10 +24,9 @@ public enum GlobalExceptionCode {
     NOT_FOUND_ACCESS_DENIED(HttpStatus.NOT_FOUND,"GLOBAL-004","접근 권한이 없음"),
 
     /*  409 CONFLICT : Resource 중복  */
-    CONFLICT_ALREADY_EXIST_USERID(HttpStatus.CONFLICT,"GLOBAL-005", "이미 존재하는 USER ID"),
 
     /*  500 INTERNAL_SERVER_ERROR : 서버 에러  */
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"GLOBAL-006", "내부 서버 에러")
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"GLOBAL-005", "내부 서버 에러")
     ;
 
     private final HttpStatus httpStatus;
