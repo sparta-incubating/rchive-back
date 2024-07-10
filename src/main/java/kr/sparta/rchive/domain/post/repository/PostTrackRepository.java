956ec8fc392391816fece5dcd724ddfe930b0283
Mayst1231 <ghkdrbwjd369@naver.com>
2024-07-10 18:41:07 +0900

    [#70] fix(Post): Entity 변경으로 인한 PostTrack Entity 삭제

--- src/main/java/kr/sparta/rchive/domain/post/repository/PostTrackRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.sparta.rchive.domain.post.repository;
-
-import kr.sparta.rchive.domain.post.entity.PostTrack;
-import kr.sparta.rchive.domain.post.entity.PostTrackId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostTrackRepository extends JpaRepository<PostTrack, PostTrackId> {
-    PostTrack findByPostId(Long postId);
-}
