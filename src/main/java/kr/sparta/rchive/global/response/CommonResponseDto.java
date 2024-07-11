1fe84773709acfd45178bfe69843f72a11350863
jiisuniui <dudndi9752@gmail.com>
2024-07-11 11:36:11 +0900

    [#64] feat: exceptionCode 수정

--- src/main/java/kr/sparta/rchive/global/response/CommonResponseDto.java
+++ src/main/java/kr/sparta/rchive/global/response/CommonResponseDto.java
@@ -1,6 +1,7 @@
 package kr.sparta.rchive.global.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import kr.sparta.rchive.global.execption.CustomException;
 import kr.sparta.rchive.global.execption.GlobalExceptionCode;
 import lombok.AllArgsConstructor;
@@ -13,17 +14,47 @@ import lombok.NoArgsConstructor;
 public class CommonResponseDto<T> {
 
     private int status;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String responseCode;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String exceptionCode;
+
     private String message;
+
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private T data;
 
-    public CommonResponseDto (int status, String message){
+    public CommonResponseDto(int value, String message) {
+        this.status = value;
+        this.message = message;
+    }
+
+    public CommonResponseDto (int status, String message, T data){
         this.status = status;
         this.message = message;
     }
 
+    public CommonResponseDto(int value, String exceptionCode, String message) {
+        this.status = value;
+        this.exceptionCode = exceptionCode;
+        this.message = message;
+    }
+
+    public CommonResponseDto (int status, String exceptionCode, String message, T data){
+        this.status = status;
+        this.exceptionCode = exceptionCode;
+        this.message = message;
+    }
+
+
     public static <T> CommonResponseDto<T> of(int status, String message, T data) {
         return new CommonResponseDto<T>(status,message,data);
+    }
+
+    public static <T> CommonResponseDto<T> of(int status, String exceptionCode, String message, T data) {
+        return new CommonResponseDto<T>(status,exceptionCode,message,data);
     }
 
     public static <T> CommonResponseDto<T> of(ResponseCode responseCode, T data) {
@@ -44,6 +75,7 @@ public class CommonResponseDto<T> {
     public static <T> CommonResponseDto<T> of(GlobalExceptionCode responseCode) {
         return new CommonResponseDto<T>(
                 responseCode.getHttpStatus().value(),
+                responseCode.getErrorCode(),
                 responseCode.getMessage()
         );
     }
@@ -51,6 +83,7 @@ public class CommonResponseDto<T> {
     public static <T> CommonResponseDto<T> of(CustomException responseCode) {
         return new CommonResponseDto<T>(
                 responseCode.getHttpStatus().value(),
+                responseCode.getErrorCode(),
                 responseCode.getMessage()
         );
     }
