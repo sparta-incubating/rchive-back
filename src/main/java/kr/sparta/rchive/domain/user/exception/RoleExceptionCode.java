5e31893ebd581ab1e7c3bd1bebe842bc3408be00
jiisuniui <dudndi9752@gmail.com>
2024-07-11 17:22:31 +0900

    [#85] feat(Role): 마지막에 선택한 권한 조회 - 백오피스

--- src/main/java/kr/sparta/rchive/domain/user/exception/RoleExceptionCode.java
+++ src/main/java/kr/sparta/rchive/domain/user/exception/RoleExceptionCode.java
@@ -8,10 +8,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum RoleExceptionCode {
     /*  400 BAD_REQUEST : 잘못된 요청  */
+    BAD_REQUEST_NO_ROLE(HttpStatus.BAD_REQUEST, "ROLE-003", "유저가 속한 트랙 없음"),
 
     /*  401 UNAUTHORIZED : 인증 안됨  */
 
     /*  403 FORBIDDEN : 권한 없음  */
+    FORBIDDEN_USER_ROLE_USER(HttpStatus.FORBIDDEN, "ROLE-002", "일반 유저 접근 불가"),
 
     /*  404 NOT_FOUND : Resource 권한 없음, Resource 를 찾을 수 없음  */
     NOT_FOUND_ROLE_REQUEST(HttpStatus.NOT_FOUND, "ROLE-001", "권한 신청 없음")
