a5f3c392a03ffea4e6c2d3d6e770a8a342f06d00
Mayst1231 <ghkdrbwjd369@naver.com>
2024-07-12 17:07:51 +0900

    [#99] feat(Post): 게시물 공개 여부 상태 비공개로 변경하는 로직 추가

--- src/main/java/kr/sparta/rchive/domain/post/response/PostResponseCode.java
+++ src/main/java/kr/sparta/rchive/domain/post/response/PostResponseCode.java
@@ -19,7 +19,8 @@ public enum PostResponseCode implements ResponseCode {
     OK_CREATE_TAG(HttpStatus.OK, "POST-009", "태그 생성 성공"),
     OK_SEARCH_TAG(HttpStatus.OK, "POST-010", "태그 검색 성공"),
     OK_SEARCH_POST_BY_TAG(HttpStatus.OK, "POST-011", "태그를 이용한 교육자료 검색 성공"),
-    OK_OPEN_POST(HttpStatus.OK, "POST-014", "게시물 공개 여부 공개로 변경");
+    OK_OPEN_POST(HttpStatus.OK, "POST-014", "게시물 공개 여부 공개로 변경"),
+    OK_CLOSE_POST(HttpStatus.OK, "POST-015", "게시물 공개 여부 비공개로 변경");
 
     private final HttpStatus httpStatus;
     private final String responseCode;
