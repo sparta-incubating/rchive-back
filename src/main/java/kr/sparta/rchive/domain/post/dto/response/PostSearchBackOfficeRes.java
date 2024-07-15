23517be69c91b13c8c487fba21a81207a2617f38
Mayst1231 <ghkdrbwjd369@naver.com>
2024-07-15 16:04:26 +0900

    [#96] feat(BackOffice): BackOffice에서 검색하는 기능에 필요한 Response 추가

--- src/main/java/kr/sparta/rchive/domain/post/dto/response/PostSearchBackOfficeRes.java
+++ src/main/java/kr/sparta/rchive/domain/post/dto/response/PostSearchBackOfficeRes.java
@@ -1,19 +1,19 @@
 package kr.sparta.rchive.domain.post.dto.response;
 
-import java.time.LocalDate;
-import java.util.List;
-import kr.sparta.rchive.domain.post.entity.Tag;
 import kr.sparta.rchive.domain.post.enums.PostTypeEnum;
 import lombok.Builder;
 
-@Builder
-public record PostSearchByTagRes(
-        String title,
-        String tutor,
-        LocalDate uploadedAt,
-        PostTypeEnum postType,
-        List<String> tagNameList
-        // 제목 카테고리 튜터 기수 공개여부 날짜 태그
-) {
+import java.time.LocalDate;
+import java.util.List;
 
+@Builder
+public record PostSearchBackOfficeRes(
+        String title,
+        PostTypeEnum postType,
+        String tutor,
+        Integer period,
+        Boolean isOpened,
+        LocalDate uploadedAt,
+        List<String> tagNameList
+) {
 }
