75ed1ee411a00d32716f7a6ac4c48580d5aa3e01
Mayst1231 <ghkdrbwjd369@naver.com>
2024-07-19 14:44:00 +0900

    [#126] feat(S3): S3 파일 업로드 Service 수정

--- src/main/java/kr/sparta/rchive/global/s3/S3ImageService.java
+++ src/main/java/kr/sparta/rchive/global/s3/S3ImageService.java
@@ -22,22 +22,29 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class S3ImageService {
-    private static final String thumbnailDirectoryName = "thumbnails/dev";
+    private static final String thumbnailDirectoryName = "thumbnails/dev/";
+    private static final String defaultThumbNailUrl = "https://rchive-bucket.s3.ap-northeast-2.amazonaws.com/thumbnails/dev/416b9c59-6728-412c-b93d-d86d57a13def-default%20Thumbnail.png";
     private final AmazonS3 amazonS3;
     @Value("${cloud.aws.s3.bucket}")
-    private String originalName;
+    private String bucketName;
 
     @Transactional
-    public String getUrlAfterUpload(MultipartFile image) {
-        String originalName = image.getOriginalFilename();
-        String extention = validateImageFile(originalName);
+    public String getUrlAfterThumbnailUpload(MultipartFile image) {
+        String originalName;
+        try{
+            originalName = image.getOriginalFilename();
+        } catch (NullPointerException e){
+            return defaultThumbNailUrl;
+        }
+
+        validateImageFile(originalName);
 
         String randomImageName = getRandomImageName(originalName);
         log.info("s3 upload name : {}", randomImageName);
 
         try {
             PutObjectRequest request = new PutObjectRequest(
-                    this.originalName,
+                    bucketName,
                     randomImageName,
                     image.getInputStream(),
                     createObjectMetadata(originalName)
@@ -48,25 +55,24 @@ public class S3ImageService {
             throw new GlobalCustomException(GlobalExceptionCode.INTERNAL_SERVER_ERROR_IMAGE_UPLOAD_FAIL);
         }
 
-        String url = amazonS3.getUrl(this.originalName, randomImageName).toString();
+        String url = amazonS3.getUrl(bucketName, randomImageName).toString();
         log.info("s3 url : {}", url);
         return url;
     }
 
-    private String validateImageFile(String originalName) {
-        int lastDotIndex = this.originalName.lastIndexOf(".");
+    private void validateImageFile(String originalName) {
+        int lastDotIndex = originalName.lastIndexOf(".");
         if (lastDotIndex == -1) {
             throw new GlobalCustomException(GlobalExceptionCode.BAD_REQUEST_IMAGE_EXTENSION_NOT_EXIST);
         }
 
-        String extention = this.originalName.substring(lastDotIndex + 1).toLowerCase();
+        String extention = originalName.substring(lastDotIndex + 1).toLowerCase();
         List<String> allowedExtentionList = Arrays.asList("jpg", "jpeg", "png", "bmp");
 
         if (!allowedExtentionList.contains(extention)) {
             throw new GlobalCustomException(GlobalExceptionCode.BAD_REQUEST_IMAGE_EXTENSION_MISMATCH);
         }
 
-        return extention;
     }
 
     private String getRandomImageName(String originalName) {
