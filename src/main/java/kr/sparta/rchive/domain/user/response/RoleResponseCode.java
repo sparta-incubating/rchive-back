de9b8ab61b523678f4b47588318cf52460eeb81b
jiisuniui <dudndi9752@gmail.com>
2024-07-22 16:52:23 +0900

    [#146] feat(Role): 내 권한(트랙 및 기수) 선택

--- src/main/java/kr/sparta/rchive/domain/user/response/RoleResponseCode.java
+++ src/main/java/kr/sparta/rchive/domain/user/response/RoleResponseCode.java
@@ -11,10 +11,10 @@ public enum RoleResponseCode implements ResponseCode {
     // OK 200
     OK_GET_MY_ROLE_LIST(HttpStatus.OK, "ROLE-001", "내 권한 조회 성공"),
     OK_REQUEST_ROLE(HttpStatus.OK, "ROLE-002", "내 권한 요청 성공"),
-    OK_GET_TRACK_NAME(HttpStatus.OK, "ROLE-005", "트랙명 조회 성공"),
-    OK_GET_TRACK_PERIOD(HttpStatus.OK, "ROLE-006", "트랙의 기수 조회 성공"),
-    OK_GET_RESULT_ROLE_FIRST_LOGIN(HttpStatus.OK, "ROLE-007", "권한 신청 결과 조회 성공"),
-    OK_GET_REQUEST_ROLE_FIRST_LOGIN(HttpStatus.OK, "ROLE-008", "권한 신청 여부 조회 성공"),
+    OK_GET_TRACK_NAME(HttpStatus.OK, "ROLE-004", "트랙명 조회 성공"),
+    OK_GET_TRACK_PERIOD(HttpStatus.OK, "ROLE-005", "트랙의 기수 조회 성공"),
+    OK_GET_RESULT_ROLE_FIRST_LOGIN(HttpStatus.OK, "ROLE-006", "권한 신청 결과 조회 성공"),
+    OK_GET_REQUEST_ROLE_FIRST_LOGIN(HttpStatus.OK, "ROLE-007", "권한 신청 여부 조회 성공"),
     ;
 
     private final HttpStatus httpStatus;
