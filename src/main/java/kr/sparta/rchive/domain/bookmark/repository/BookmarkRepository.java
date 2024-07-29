22870c1e954f6ed537bdd392b219869e825d9860
Mayst1231 <ghkdrbwjd369@naver.com>
2024-07-29 14:08:55 +0900

    [#189] feat(Bookmark): Profile에서 Bookmark에 등록한 게시물 볼 수 있도록 Repository 추가

--- src/main/java/kr/sparta/rchive/domain/bookmark/repository/BookmarkRepository.java
+++ src/main/java/kr/sparta/rchive/domain/bookmark/repository/BookmarkRepository.java
@@ -1,10 +1,11 @@
 package kr.sparta.rchive.domain.bookmark.repository;
 
-import java.util.Optional;
 import kr.sparta.rchive.domain.bookmark.entity.Bookmark;
 import kr.sparta.rchive.domain.bookmark.entity.BookmarkId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookmarkRepository extends JpaRepository<Bookmark, BookmarkId> {
+import java.util.Optional;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, BookmarkId>, BookmarkRepositoryCustom {
     Optional<Bookmark> findBookmarkByUserIdAndPostId(Long userId, Long postId);
 }
