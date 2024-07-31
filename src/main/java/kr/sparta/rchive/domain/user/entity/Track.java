33366a84136aaf1cd535f872bd9e49595dba26e9
Mayst1231 <ghkdrbwjd369@naver.com>
2024-07-31 14:38:27 +0900

    [#195] feat(BackOffice): 유저 트랙 열람 권한 거절 메소드 Track 클래스에 추가

--- src/main/java/kr/sparta/rchive/domain/user/entity/Track.java
+++ src/main/java/kr/sparta/rchive/domain/user/entity/Track.java
@@ -51,4 +51,8 @@ public class Track extends BaseTimeEntity {
     public void trackPermissionTrue() {
         this.isPermission = true;
     }
+
+    public void trackPermissionFalse() {
+        this.isPermission = false;
+    }
 }
