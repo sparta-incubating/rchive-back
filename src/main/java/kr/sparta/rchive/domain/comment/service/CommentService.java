919ebc3993244173bd7b8b818e2c2aaceeea185d
Mayst1231 <ghkdrbwjd369@naver.com>
2024-08-05 13:11:11 +0900

    [#212] feat(Comment): Comment 추가하는 Service 추가

--- src/main/java/kr/sparta/rchive/domain/comment/service/CommentService.java
+++ src/main/java/kr/sparta/rchive/domain/comment/service/CommentService.java
@@ -1,15 +1,23 @@
 package kr.sparta.rchive.domain.comment.service;
 
+import kr.sparta.rchive.domain.comment.dto.request.CommentCreateReq;
 import kr.sparta.rchive.domain.comment.dto.response.CommentRes;
+import kr.sparta.rchive.domain.comment.entity.Comment;
 import kr.sparta.rchive.domain.comment.repository.CommentRepository;
+import kr.sparta.rchive.domain.post.entity.Post;
+import kr.sparta.rchive.domain.post.exception.PostCustomException;
+import kr.sparta.rchive.domain.post.exception.PostExceptionCode;
+import kr.sparta.rchive.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CommentService {
 
     private final CommentRepository commentRepository;
@@ -22,5 +30,22 @@ public class CommentService {
                         .createdAt(comment.getCreatedAt())
                         .build())
                 .collect(Collectors.toList());
+    }
+
+    public void createComment(User user, Post post, Comment comment, CommentCreateReq request) {
+        Comment createComment = Comment.builder()
+                .content(request.content())
+                .post(post)
+                .user(user)
+                .parentComment(comment)
+                .build();
+
+        commentRepository.save(createComment);
+    }
+
+    public Comment findCommentByCommentId(Long commentId) {
+        return commentRepository.findById(commentId).orElseThrow(
+                () -> new PostCustomException(PostExceptionCode.NOT_FOUND_COMMENT)
+        );
     }
 }
---
+++ src/main/java/kr/sparta/rchive/domain/core/service/PostCommentCoreService.java
@@ -0,0 +1,34 @@
+package kr.sparta.rchive.domain.core.service;
+
+import kr.sparta.rchive.domain.comment.dto.request.CommentCreateReq;
+import kr.sparta.rchive.domain.comment.entity.Comment;
+import kr.sparta.rchive.domain.comment.service.CommentService;
+import kr.sparta.rchive.domain.post.entity.Post;
+import kr.sparta.rchive.domain.post.service.PostService;
+import kr.sparta.rchive.domain.user.entity.User;
+import kr.sparta.rchive.domain.user.service.TrackService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PostCommentCoreService {
+
+    private final CommentService commentService;
+    private final PostService postService;
+    private final TrackService trackService;
+
+    @Transactional
+    public void createComment(User user, Long postId, Long parentCommentId, CommentCreateReq request) {
+        Post findPost = postService.findPostById(postId);
+
+        Comment findComment = null;
+
+        if(parentCommentId != null) {
+            findComment = commentService.findCommentByCommentId(parentCommentId);
+        }
+
+        commentService.createComment(user, findPost, findComment, request);
+    }
+}
