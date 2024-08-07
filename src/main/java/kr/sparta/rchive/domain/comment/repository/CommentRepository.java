22939a7ee73f76d052592ea95e975e5883afc89a
Mayst1231 <ghkdrbwjd369@naver.com>
2024-08-07 16:02:13 +0900

    [#226] feat(Comment): 대댓글 조회 Repository 추가

--- src/main/java/kr/sparta/rchive/domain/comment/repository/CommentRepository.java
+++ src/main/java/kr/sparta/rchive/domain/comment/repository/CommentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
-
+    List<Comment> findByParentCommentIdAndIsDeletedFalse(Long parentCommentId);
 }
