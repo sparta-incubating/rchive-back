537bf3a6c12998e26b224bb37c789100f3d5de5b
jiisuniui <dudndi9752@gmail.com>
2024-08-08 11:12:08 +0900

    [#202] feat: cors 허용 origin 수정

--- src/main/java/kr/sparta/rchive/global/config/WebMvcConfig.java
+++ src/main/java/kr/sparta/rchive/global/config/WebMvcConfig.java
@@ -6,10 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","http://rchive.kr", "https://rchive.kr")
+                .allowedOrigins("http://localhost:3000",
+                        "http://rchive.kr", "http://www.rchive.kr", "http://dev.rchive.kr",
+                        "https://rchive.kr", "https://www.rchive.kr", "https://dev.rchive.kr")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
