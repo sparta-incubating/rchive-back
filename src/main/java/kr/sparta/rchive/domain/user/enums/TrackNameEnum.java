0ebdfce7d1747705ccd6dc02bca54c68da57c20e
jiisuniui <dudndi9752@gmail.com>
2024-08-12 14:41:14 +0900

    [#246] feat: TrackNameEnum 변경

--- src/main/java/kr/sparta/rchive/domain/user/enums/TrackNameEnum.java
+++ src/main/java/kr/sparta/rchive/domain/user/enums/TrackNameEnum.java
@@ -6,5 +6,5 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum TrackNameEnum {
-    UNITY, NODEJS, SPRING_JAVA, SPRING_KOTLIN, FRONT_REACT, WEB, ANDROID, IOS, DATA, UXUI;
+    ANDROID, DATA, REACT, IOS, NODEJS, SPRING_JAVA, SPRING_KOTLIN, UNITY, UXUI, AI, SPRING_DEEP;
 }
