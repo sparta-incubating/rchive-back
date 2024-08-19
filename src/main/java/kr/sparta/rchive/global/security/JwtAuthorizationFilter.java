58cf3689b21a00e61a965ae8403e1369d394853d
Mayst1231 <ghkdrbwjd369@naver.com>
2024-08-19 18:12:22 +0900

    [#277] feat(user): access Token 만료 여부 체크하는 url 필터 처리 변경

--- src/main/java/kr/sparta/rchive/global/security/JwtAuthorizationFilter.java
+++ src/main/java/kr/sparta/rchive/global/security/JwtAuthorizationFilter.java
@@ -26,6 +26,13 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
+        String path = request.getRequestURI();
+
+        if ("/apis/v1/users/token/expired".equals(path)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String accessAuth = request.getHeader("Authorization");
 
         if (accessAuth == null || !accessAuth.startsWith("Bearer ")) {
