a4babbc4fcb5671d2d35b3b9503f293b7c68d082
Mayst1231 <ghkdrbwjd369@naver.com>
2024-08-21 17:00:24 +0900

    [#290] feat(BackOffice): 백오피스에서 게시물 수정 시 기존 게시물 내용 조회하는 기능 Response Dto 추가

---
+++ src/main/java/kr/sparta/rchive/domain/post/dto/response/PostModifyPreviewRes.java
@@ -0,0 +1,23 @@
+package kr.sparta.rchive.domain.post.dto.response;
+
+import kr.sparta.rchive.domain.post.dto.TagInfo;
+import kr.sparta.rchive.domain.post.enums.PostTypeEnum;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Builder
+public record PostModifyPreviewRes(
+        String thumbnailUrl,
+        String title,
+        String contentLink,
+        String videoLink,
+        Integer period,
+        PostTypeEnum postType,
+        TutorRes tutorRes,
+        List<String> tagNameList,
+        LocalDate uploadedAt,
+        Boolean isOpened
+) {
+}
