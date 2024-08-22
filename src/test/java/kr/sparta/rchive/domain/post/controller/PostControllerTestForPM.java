c1785e36f0ddb8e60a75d0b8c5e8863861c0005d
Mayst1231 <ghkdrbwjd369@naver.com>
2024-08-22 16:19:07 +0900

    [#299] test(post): 게시물 생성 기능 성공 테스트 추가

---
+++ src/test/java/kr/sparta/rchive/domain/post/controller/PostControllerTestForPM.java
@@ -0,0 +1,101 @@
+package kr.sparta.rchive.domain.post.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.sparta.rchive.domain.comment.service.CommentService;
+import kr.sparta.rchive.domain.core.service.PostBookmarkCoreService;
+import kr.sparta.rchive.domain.core.service.PostCommentCoreService;
+import kr.sparta.rchive.domain.core.service.PostTagCoreService;
+import kr.sparta.rchive.domain.post.dto.request.PostCreateReq;
+import kr.sparta.rchive.domain.post.dto.response.PostCreateRes;
+import kr.sparta.rchive.domain.post.enums.PostTypeEnum;
+import kr.sparta.rchive.domain.post.service.TagService;
+import kr.sparta.rchive.domain.user.entity.User;
+import kr.sparta.rchive.domain.user.enums.TrackNameEnum;
+import kr.sparta.rchive.security.WithMockCustomPM;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDate;
+
+import static org.mockito.BDDMockito.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = { PostController.class })
+@ActiveProfiles("test")
+@WithMockCustomPM
+public class PostControllerTestForPM {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext context;
+    @Autowired
+    private ObjectMapper obj;
+    @MockBean
+    private PostTagCoreService postTagCoreService;
+    @MockBean
+    private PostBookmarkCoreService postBookmarkCoreService;
+    @MockBean
+    private PostCommentCoreService postCommentCoreService;
+    @MockBean
+    private TagService tagService;
+    @MockBean
+    private CommentService commentService;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .alwaysDo(print())
+                .build();
+    }
+
+    @Test
+    public void 관리자_게시물_생성() throws Exception {
+        // Given
+        TrackNameEnum trackName = TrackNameEnum.ANDROID;
+        Integer period = 0;
+        PostCreateReq request = PostCreateReq.builder()
+                .postType(PostTypeEnum.Sparta_Lecture)
+                .title("Test_Post_Create")
+                .tutorId(1L)
+                .uploadedAt(LocalDate.now())
+                .thumbnailUrl("test")
+                .videoLink("test")
+                .contentLink("test")
+                .content("test")
+                .postPeriod(1)
+                .isOpened(true)
+                .build();
+
+        String json = obj.writeValueAsString(request);
+
+        PostCreateRes response = PostCreateRes.builder()
+                .postId(1L)
+                .build();
+
+        given(postTagCoreService.createPost(any(User.class), any(TrackNameEnum.class), any(Integer.class), any(PostCreateReq.class)))
+                .willReturn(response);
+        // When - Then
+        mockMvc.perform(post("/apis/v1/posts?trackName=ANDROID&loginPeriod=0")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpectAll(
+                status().isOk(),
+                jsonPath("$.message").value("교육자료 생성 성공"),
+                jsonPath("$.data.postId").value(1L)
+        );
+    }
+}
