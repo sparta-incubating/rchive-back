d2be03ccc3c55bcb49272c2235d7bb2c104ba2de
Mayst1231 <ghkdrbwjd369@naver.com>
2024-08-23 14:55:28 +0900

    [#312] test(post): 게시물 수정 로직 Controller 테스트 작성

--- src/test/java/kr/sparta/rchive/domain/post/controller/PostControllerTestForPM.java
+++ src/test/java/kr/sparta/rchive/domain/post/controller/PostControllerTestForPM.java
@@ -6,7 +6,9 @@ import kr.sparta.rchive.domain.core.service.PostBookmarkCoreService;
 import kr.sparta.rchive.domain.core.service.PostCommentCoreService;
 import kr.sparta.rchive.domain.core.service.PostTagCoreService;
 import kr.sparta.rchive.domain.post.dto.request.PostCreateReq;
+import kr.sparta.rchive.domain.post.dto.request.PostUpdateReq;
 import kr.sparta.rchive.domain.post.dto.response.PostCreateRes;
+import kr.sparta.rchive.domain.post.dto.response.PostModifyRes;
 import kr.sparta.rchive.domain.post.enums.PostTypeEnum;
 import kr.sparta.rchive.domain.post.service.TagService;
 import kr.sparta.rchive.domain.user.entity.User;
@@ -27,6 +29,7 @@ import java.time.LocalDate;
 
 import static org.mockito.BDDMockito.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -65,8 +68,6 @@ public class PostControllerTestForPM {
     @Test
     public void 관리자_게시물_생성() throws Exception {
         // Given
-        TrackNameEnum trackName = TrackNameEnum.ANDROID;
-        Integer period = 0;
         PostCreateReq request = PostCreateReq.builder()
                 .postType(PostTypeEnum.Sparta_Lecture)
                 .title("Test_Post_Create")
@@ -95,6 +96,43 @@ public class PostControllerTestForPM {
         ).andExpectAll(
                 status().isOk(),
                 jsonPath("$.message").value("교육자료 생성 성공"),
+                jsonPath("$.data.postId").value(1L)
+        );
+    }
+
+    @Test
+    public void 관리자_게시물_수정() throws Exception {
+        // Given
+        Long postId = 1L;
+        PostUpdateReq request = PostUpdateReq.builder()
+                .postType(PostTypeEnum.Sparta_Lecture)
+                .title("change test")
+                .tutorId(1L)
+                .uploadedAt(LocalDate.now())
+                .thumbnailUrl("change test")
+                .videoLink("change test")
+                .contentLink("change test")
+                .content("change test")
+                .updatePeriod(2)
+                .isOpened(true)
+                .build();
+
+        String json = obj.writeValueAsString(request);
+
+        PostModifyRes response = PostModifyRes.builder()
+                .postId(1L)
+                .build();
+
+        given(postTagCoreService.updatePost(any(User.class), any(TrackNameEnum.class), any(Integer.class),
+                any(Long.class), any(PostUpdateReq.class))).willReturn(response);
+
+        // When - Then
+        mockMvc.perform(patch("/apis/v1/posts/{postId}?trackName=ANDROID&loginPeriod=0", postId)
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpectAll(
+                status().isOk(),
+                jsonPath("$.message").value("교육자료 수정 성공"),
                 jsonPath("$.data.postId").value(1L)
         );
     }
