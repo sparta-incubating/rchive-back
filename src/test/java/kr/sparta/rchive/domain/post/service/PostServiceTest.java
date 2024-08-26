ebdd1779d77e558aae2a9a88c3d299f314bdac09
Mayst1231 <ghkdrbwjd369@naver.com>
2024-08-26 17:36:45 +0900

    [#329] test(post): 게시물 업데이트 서비스 로직 성공 테스트

--- src/test/java/kr/sparta/rchive/domain/post/service/PostServiceTest.java
+++ src/test/java/kr/sparta/rchive/domain/post/service/PostServiceTest.java
@@ -1,11 +1,13 @@
 package kr.sparta.rchive.domain.post.service;
 
 import kr.sparta.rchive.domain.post.dto.request.PostCreateReq;
+import kr.sparta.rchive.domain.post.dto.request.PostUpdateReq;
 import kr.sparta.rchive.domain.post.entity.Post;
 import kr.sparta.rchive.domain.post.entity.Tutor;
 import kr.sparta.rchive.domain.post.enums.PostTypeEnum;
 import kr.sparta.rchive.domain.post.repository.PostRepository;
 import kr.sparta.rchive.domain.user.entity.Track;
+import kr.sparta.rchive.domain.user.enums.TrackNameEnum;
 import kr.sparta.rchive.test.PostTest;
 import kr.sparta.rchive.test.TrackTest;
 import kr.sparta.rchive.test.TutorTest;
@@ -52,6 +54,34 @@ public class PostServiceTest implements PostTest, TrackTest, TutorTest {
 
         // When
         postService.createPost(request, track, tutor);
+
+        // Then
+        verify(postRepository, times(1)).save(any(Post.class));
+    }
+
+    @Test
+    @DisplayName("게시물 업데이트하는 서비스 로직 성공 테스트")
+    void 게시물_업데이트_서비스_성공_테스트() {
+        // Given
+        Post post = TEST_POST;
+        Track track = TEST_TRACK_ANDROID;
+        Tutor tutor = TEST_TUTOR;
+
+        PostUpdateReq request = PostUpdateReq.builder()
+                .postType(PostTypeEnum.Sparta_Lecture)
+                .title("Test")
+                .tutorId(TEST_TUTOR_ID)
+                .uploadedAt(LocalDate.now())
+                .thumbnailUrl("Test")
+                .videoLink("Test")
+                .content("Test")
+                .contentLink("Test")
+                .trackName(TrackNameEnum.ANDROID)
+                .updatePeriod(1)
+                .isOpened(true)
+                .build();
+        // When
+        postService.updatePost(post, request, track, tutor);
 
         // Then
         verify(postRepository, times(1)).save(any(Post.class));
