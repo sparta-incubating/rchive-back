d2405cb1487fe361a3172b995af5036fd56ee910
jiisuniui <dudndi9752@gmail.com>
2024-08-26 10:20:14 +0900

    [#324] feat(Profile): 프로필 변경 로직 수정

---
+++ src/main/java/kr/sparta/rchive/domain/user/dto/request/ProfileUpdateNicknameReq.java
@@ -0,0 +1,12 @@
+package kr.sparta.rchive.domain.user.dto.request;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+
+@Builder
+public record ProfileUpdateNicknameReq(
+        @Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리")
+        String nickname
+) {
+
+}
