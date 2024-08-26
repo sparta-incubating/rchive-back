ff8e0fffc9af1af3da7aa2286bfd69aa81d1f61c
Mayst1231 <ghkdrbwjd369@naver.com>
2024-08-26 17:49:59 +0900

    [#329] test(post): 교육자료를 교육자료의 ID를 이용하여 검색하는 서비스 로직 실패 테스트

--- src/test/java/kr/sparta/rchive/domain/post/service/PostServiceTest.java
+++ src/test/java/kr/sparta/rchive/domain/post/service/PostServiceTest.java
@@ -5,6 +5,7 @@ import kr.sparta.rchive.domain.post.dto.request.PostUpdateReq;
 import kr.sparta.rchive.domain.post.entity.Post;
 import kr.sparta.rchive.domain.post.entity.Tutor;
 import kr.sparta.rchive.domain.post.enums.PostTypeEnum;
+import kr.sparta.rchive.domain.post.exception.PostCustomException;
 import kr.sparta.rchive.domain.post.repository.PostRepository;
 import kr.sparta.rchive.domain.user.entity.Track;
 import kr.sparta.rchive.domain.user.enums.TrackNameEnum;
@@ -18,11 +19,13 @@ import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.times;
@@ -117,5 +120,22 @@ public class PostServiceTest implements PostTest, TrackTest, TutorTest {
         assertThat(post.getTitle()).isEqualTo(TEST_POST.getTitle());
         assertThat(post.getThumbnailUrl()).isEqualTo(TEST_POST.getThumbnailUrl());
         assertThat(post.getContent()).isEqualTo(TEST_POST.getContent());
+    }
+
+    @Test
+    @DisplayName("교육자료를 교육자료의 ID를 이용하여 검색하는 서비스 로직 실패 테스트")
+    void 게시물_아이디로_게시물_찾아오는_서비스_실패_테스트() {
+        // Given
+        given(postRepository.findById(any(Long.class))).willReturn(Optional.empty());
+
+        // When
+        PostCustomException exception = assertThrows(
+                PostCustomException.class, () -> postService.findPostById(any(Long.class))
+        );
+
+        // Then
+        assertThat(exception.getErrorCode()).isEqualTo("POST-4001");
+        assertThat(exception.getMessage()).isEqualTo("자료를 찾을 수 없음");
+        assertThat(exception.getHttpStatus()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 }
