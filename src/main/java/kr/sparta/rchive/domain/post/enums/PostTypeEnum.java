fd07422aa7f75a1f7f91d509cb04f925bf5a392c
Mayst1231 <ghkdrbwjd369@naver.com>
2024-09-11 15:32:02 +0900

    [#371] refactor(post): postType 이름 지정

--- src/main/java/kr/sparta/rchive/domain/post/enums/PostTypeEnum.java
+++ src/main/java/kr/sparta/rchive/domain/post/enums/PostTypeEnum.java
@@ -1,11 +1,18 @@
 package kr.sparta.rchive.domain.post.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum PostTypeEnum {
-    Sparta_Lecture,
-    Special_Lecture,
-    Level_Challenge,
-    Level_Standard,
-    Level_Basic,
-    Level_All,
-    Project_Description
+    Sparta_Lecture("강의자료"),
+    Special_Lecture("특강/실시간 세션"),
+    Level_Challenge("챌린지"),
+    Level_Standard("스탠다드"),
+    Level_Basic("베이직"),
+    Level_All("수준별강의"),
+    Project_Description("과제해설");
+
+     private final String name;
 }
