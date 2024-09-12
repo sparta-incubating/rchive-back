fff0d1df345b460b20d712144a0ed514109c30e0
jiisuniui <dudndi9752@gmail.com>
2024-09-12 14:13:22 +0900

    [#379] refactor(Swagger): UserLoginReq DTO 예시 추가

--- src/main/java/kr/sparta/rchive/domain/user/dto/request/UserLoginReq.java
+++ src/main/java/kr/sparta/rchive/domain/user/dto/request/UserLoginReq.java
@@ -1,14 +1,18 @@
 package kr.sparta.rchive.domain.user.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @JsonIgnoreProperties
 public record UserLoginReq(
+        @Schema(description = "이메일", example = "email@email.com")
         String username,
+        
+        @Schema(description = "비밀번호", example = "user123")
         String password
-        ) {
+) {
 
 }
