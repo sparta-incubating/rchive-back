d5fd6e6412218bc10e8c007e76ce85b5116d2359
jiisuniui <dudndi9752@gmail.com>
2024-09-12 10:51:02 +0900

    [#379] refactor(Swagger): UserSignupReq DTO 예시 추가

--- src/main/java/kr/sparta/rchive/domain/user/dto/request/UserSignupReq.java
+++ src/main/java/kr/sparta/rchive/domain/user/dto/request/UserSignupReq.java
@@ -1,6 +1,7 @@
 package kr.sparta.rchive.domain.user.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import java.time.LocalDate;
@@ -13,23 +14,50 @@ import lombok.Builder;
 @JsonIgnoreProperties
 public record UserSignupReq(
         //String oAuthId,
+        @Schema(description = "oAuthType", example = "LOCAL")
         OAuthTypeEnum oAuthType,
+
+        @Schema(description = "이메일", example = "email@email.com")
         @Email(message = "올바르지 않은 이메일 형식")
         String email,
+
+        @Schema(description = "비밀번호", example = "user123")
         @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=\\S+$).{6,20}$", message = "비밀번호는 6~20자 영문자와 숫자를 포함해야 합니다")
         String password,
+
+        @Schema(description = "이름", example = "아무개")
         String username,
+
+        @Schema(description = "생년월일", example = "2000-01-01")
         LocalDate birth,
+
+        @Schema(description = "휴대폰 번호", example = "01012345678")
         @Pattern(regexp = "^[0-9]{11}$", message = "휴대폰번호는 숫자만 11자리")
         String phone,
+
+        @Schema(description = "성별", example = "NONE")
         GenderEnum gender,
+
+        @Schema(description = "프로필 이미지", example = "default")
         String profileImg,
+
+        @Schema(description = "닉네임", example = "닉네임")
         @Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,10}$|^\\s*$|^$", message = "닉네임은 특수문자를 제외한 2~10자리 또는 빈 값(공백 포함)이어야 합니다.")
         String nickname,
+
+        @Schema(description = "유저 권한", example = "USER")
         UserRoleEnum userRole,
+
+        @Schema(description = "만 14세 이상 동의", example = "true")
         Boolean termUserAge,
+
+        @Schema(description = "서비스 약관 동의", example = "true")
         Boolean termUseService,
+
+        @Schema(description = "개인정보처리방침 및 제3자 제공 동의", example = "true")
         Boolean termPersonalInfo,
+
+        @Schema(description = "광고성 정보 수신 동의", example = "true")
         Boolean termAdvertisement
 
 ) {
