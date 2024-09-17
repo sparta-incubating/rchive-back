232a0fe6308f552405110a527642e7278c454111
jiisuniui <dudndi9752@gmail.com>
2024-09-17 22:26:47 +0900

    [#379] refactor(User): ProfileUpdatePasswordReq 작성

--- src/main/java/kr/sparta/rchive/domain/user/dto/request/ProfileUpdatePasswordReq.java
+++ src/main/java/kr/sparta/rchive/domain/user/dto/request/ProfileUpdatePasswordReq.java
@@ -1,12 +1,16 @@
 package kr.sparta.rchive.domain.user.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 
 @Builder
 public record ProfileUpdatePasswordReq(
+        @Schema(description = "기존 비밀번호", example = "user123")
         @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=\\S+$).{6,20}$", message = "비밀번호는 6~20자 영문자와 숫자를 포함해야 합니다")
         String originPassword,
+
+        @Schema(description = "새 비밀번호", example = "user123")
         @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=\\S+$).{6,20}$", message = "비밀번호는 6~20자 영문자와 숫자를 포함해야 합니다")
         String newPassword
 ) {
