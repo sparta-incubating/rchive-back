f85c1fe1a560b6c67fae1ad48c969a026bec9058
jiisuniui <dudndi9752@gmail.com>
2024-09-17 22:27:49 +0900

    [#379] refactor(User): RoleRequestReq 작성

--- src/main/java/kr/sparta/rchive/domain/user/dto/request/RoleRequestReq.java
+++ src/main/java/kr/sparta/rchive/domain/user/dto/request/RoleRequestReq.java
@@ -1,15 +1,21 @@
 package kr.sparta.rchive.domain.user.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 import kr.sparta.rchive.domain.user.enums.TrackNameEnum;
 import kr.sparta.rchive.domain.user.enums.TrackRoleEnum;
 import lombok.Builder;
 
 @Builder
 @JsonIgnoreProperties
-public record RoleRequestReq (
+public record RoleRequestReq(
+        @Schema(description = "트랙명", example = "ANDROID")
         TrackNameEnum trackName,
+
+        @Schema(description = "기수", example = "1")
         int period,
+
+        @Schema(description = "트랙 권한", example = "STUDENT")
         TrackRoleEnum trackRole
 ) {
 
