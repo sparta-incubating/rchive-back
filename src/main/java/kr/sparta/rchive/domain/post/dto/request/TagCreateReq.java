43c2e91bccc5dc4cf3c7a39774bbd1b3b9803bf0
hwang <ghkdrbwjd369@naver.com>
2024-09-24 16:52:06 +0900

    [#396] refactor(post): 기존 태그 생성 방식 변경으로 인한 수정사항 적용

--- src/main/java/kr/sparta/rchive/domain/post/dto/request/TagCreateReq.java
+++ src/main/java/kr/sparta/rchive/domain/post/dto/request/TagCreateReq.java
@@ -1,10 +1,11 @@
 package kr.sparta.rchive.domain.post.dto.request;
 
+import java.util.List;
 import lombok.Builder;
 
 @Builder
 public record TagCreateReq(
-        String tagName
+        List<String> tagNameList
 ) {
 
 }
