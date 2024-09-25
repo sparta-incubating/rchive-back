a21ff0cb69073a9e931d8d5fdb620be687c21799
Mayst1231 <ghkdrbwjd369@naver.com>
2024-09-25 17:58:45 +0900

    [#403] test(tutor): 튜터 검색하는 기능 서비스 로직 실패 테스트

--- src/test/java/kr/sparta/rchive/domain/post/service/TutorServiceTest.java
+++ src/test/java/kr/sparta/rchive/domain/post/service/TutorServiceTest.java
@@ -1,6 +1,8 @@
 package kr.sparta.rchive.domain.post.service;
 
 import kr.sparta.rchive.domain.post.entity.Tutor;
+import kr.sparta.rchive.domain.post.exception.PostCustomException;
+import kr.sparta.rchive.domain.post.exception.PostExceptionCode;
 import kr.sparta.rchive.domain.post.repository.TutorRepository;
 import kr.sparta.rchive.test.TutorTest;
 import org.junit.jupiter.api.DisplayName;
@@ -13,6 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -36,5 +39,23 @@ public class TutorServiceTest implements TutorTest {
 
         // Then
         assertThat(result.getTutorName()).isEqualTo(TEST_TUTOR.getTutorName());
+    }
+
+    @Test
+    @DisplayName("튜터 검색하는 기능 서비스 로직 실패 테스트")
+    void 튜터_검색기능_실패_테스트() {
+        // Given
+        Long tutorId = 1L;
+
+        given(tutorRepository.findById(tutorId)).willReturn(Optional.empty());
+
+        // When
+        PostCustomException exception = assertThrows(
+                PostCustomException.class, () -> tutorService.findTutorById(tutorId)
+        );
+
+        // Then
+        assertThat(exception.getErrorCode()).isEqualTo(PostExceptionCode.NOT_FOUND_TUTOR.getErrorCode());
+        assertThat(exception.getMessage()).isEqualTo(PostExceptionCode.NOT_FOUND_TUTOR.getMessage());
     }
 }
