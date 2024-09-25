52cfb35bb9ae8a86a46b2652016c18845f08374d
Mayst1231 <ghkdrbwjd369@naver.com>
2024-09-25 17:55:44 +0900

    [#403] test(tutor): 튜터 검색하는 기능 서비스 로직 성공 테스트

---
+++ src/test/java/kr/sparta/rchive/domain/post/service/TutorServiceTest.java
@@ -0,0 +1,40 @@
+package kr.sparta.rchive.domain.post.service;
+
+import kr.sparta.rchive.domain.post.entity.Tutor;
+import kr.sparta.rchive.domain.post.repository.TutorRepository;
+import kr.sparta.rchive.test.TutorTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class TutorServiceTest implements TutorTest {
+    @InjectMocks
+    private TutorService tutorService;
+
+    @Mock
+    private TutorRepository tutorRepository;
+
+    @Test
+    @DisplayName("튜터 검색하는 기능 서비스 로직 성공 테스트")
+    void 튜터_검색기능_성공_테스트() {
+        // Given
+        Long tutorId = 1L;
+
+        given(tutorRepository.findById(tutorId)).willReturn(Optional.of(TEST_TUTOR));
+
+        // When
+        Tutor result = tutorService.findTutorById(tutorId);
+
+        // Then
+        assertThat(result.getTutorName()).isEqualTo(TEST_TUTOR.getTutorName());
+    }
+}
