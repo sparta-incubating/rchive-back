b18e75b9183820daa180db84ddaf45b007578819
Mayst1231 <ghkdrbwjd369@naver.com>
2024-09-26 14:38:55 +0900

    [#380] refactor(post): DeleteThumbnailReq 예시 작성

--- src/main/java/kr/sparta/rchive/domain/post/dto/request/DeleteThumbnailReq.java
+++ src/main/java/kr/sparta/rchive/domain/post/dto/request/DeleteThumbnailReq.java
@@ -1,12 +1,15 @@
 package kr.sparta.rchive.domain.post.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import kr.sparta.rchive.domain.user.enums.TrackNameEnum;
 import lombok.Builder;
 
 @Builder
 public record DeleteThumbnailReq(
-    TrackNameEnum trackName,
-    Integer period
+        @Schema(description = "트랙 이름", example = "Android")
+        TrackNameEnum trackName,
+        @Schema(description = "기수", example = "1")
+        Integer period
 ) {
 
 }
